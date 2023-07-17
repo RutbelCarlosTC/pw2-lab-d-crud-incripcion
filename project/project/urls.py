ce72d1763c9e61694065011666bd39503c056f96
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-07-17 16:23:37 -0500

    corrigiendo errores

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls'))
 ]
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
 
